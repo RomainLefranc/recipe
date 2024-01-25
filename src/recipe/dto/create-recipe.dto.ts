fdb6c157b157b4efc5bd6a30825afb2b41b3e221
RomainLefranc <lefranc46@gmail.com>
2024-01-25 16:25:51 +0400

    add dtos info to swagger

--- src/recipe/dto/create-recipe.dto.ts
+++ src/recipe/dto/create-recipe.dto.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional } from 'class-validator';
 
 export class CreateRecipeDto {
+  @ApiProperty({
+    description: 'Name of the recipe',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    description: 'Description of the recipe',
+  })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({
+    description: 'Ingredients of the recipe',
+  })
   @IsString()
   ingredients: string;
 
+  @ApiProperty({
+    description: 'Instructions of the recipe',
+  })
   @IsString()
   instructions: string;
 }
